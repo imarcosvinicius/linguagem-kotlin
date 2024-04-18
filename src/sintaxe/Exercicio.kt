dc10404f6fe0a75f74244de274db5cfdd652703e
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2024-04-18 13:19:32 -0300

    exercício sintaxe
    Pequeno exercício sobre a sintaxe da linguagem

---
+++ src/sintaxe/Exercicio.kt
@@ -0,0 +1,12 @@
+package sintaxe
+
+fun main() {
+
+    var nome = "Jim Carrey"
+    var text: String
+    val byte = 42
+    val pib = 1_869_000_000_000
+    val populacao = 211_000_000
+    val pibPerCapita = pib / populacao
+    println("PIB per capita: $pibPerCapita")
+}
